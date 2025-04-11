e8efb503923a547ebac56d676b4616ad6af2d0c0
Katarzyna Tchir <tchir.katarzyna@gmail.com>
2025-04-11 21:12:40 +0200

    add remixicon to toggle dark mode

--- src/components/shared/ToggleThemeMode.jsx
+++ src/components/shared/ToggleThemeMode.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
+import { RiSunFill, RiMoonLine } from '@remixicon/react';
 
 const ToggleThemeMode = () => {
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+
   return (
     <button
       onClick={toggleDarkMode}
-      className="w-12 h-12 bg-neutral-900 dark:bg-white  rounded-full text-white dark:text-black font-semibold text-sm cursor-pointer"
+      className="cursor-pointer transition-transform duration-300 transform hover:rotate-90"
     >
-      {darkMode ? 'Light' : 'Dark'}
+      {darkMode ? <RiSunFill /> : <RiMoonLine />}
     </button>
   );
 };

be62bc55fac3acee500ebbda9552e625ebf5f175
Katarzyna Tchir <tchir.katarzyna@gmail.com>
2025-04-11 21:30:16 +0200

    create main nav and organize header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   return (
-    <header className="bg-white dark:bg-neutral-800 text-black dark:text-neutral-100 px-12 py-6 flex justify-between items-center shadow-md ">
-      <Link
-        to="/"
-        className="text-2xl font-bold text-pokeblue-500 dark:text-neutral-200"
-      >
-        Pokedex
-      </Link>
-      <div className="flex flex-col items-end">
+    <header className="bg-white dark:bg-neutral-800 text-black dark:text-neutral-100 px-12 py-6  shadow-md ">
+      <div className="flex justify-between">
+        <Link
+          to="/"
+          className="text-2xl font-bold text-pokeblue-500 dark:text-neutral-200 mr-8"
+        >
+          Pokedex
+        </Link>
         <div className="flex justify-end items-center space-x-6 mb-4">
           {userLoggedIn ? (
             <p>Witaj, {userLoggedIn.name}!</p>
@@ -26,54 +26,72 @@ const Header = () => {
 
           <ToggleThemeMode />
         </div>
-
-        {/* TODO: na mobile powinien być hamburger */}
-        {/* TODO 2: wynieść NAV do osobnego komponentu, bo zobił się bałagan!! */}
-        <nav className="flex space-x-3">
-          <Link
-            to="/favourites"
-            className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
-          >
-            Ulubione
-          </Link>
-          <Link
-            to="/arena"
-            className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
-          >
-            Arena
-          </Link>
-          {userLoggedIn ? (
-            <Link
-              to="/"
-              className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
-              onClick={() => {
-                setUserLoggedIn(null);
-                localStorage.removeItem('loggedUser');
-                enqueueSnackbar('Wylogowano pomyślnie', { variant: 'info' });
-              }}
-            >
-              Wyloguj
-            </Link>
-          ) : (
-            <>
-              <Link
-                to="/register"
-                className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
-              >
-                Rejestracja
-              </Link>
-              <Link
-                to="/login"
-                className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
-              >
-                Logowanie
-              </Link>
-            </>
-          )}
-
-          {/* PAMIĘTAJ o zablokowaniu przejścia pod adresy dla nizalogowanego użytkownika: Ulubione, Arena, Ranking, Edycja */}
-        </nav>
       </div>
+
+      {/* TODO: na mobile powinien być hamburger */}
+      {/* TODO 2: wynieść NAV do osobnego komponentu, bo zobił się bałagan!! */}
+      <nav className="flex flex-wrap gap-3 justify-end">
+        <Link
+          to="/favourites"
+          className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+        >
+          Ulubione
+        </Link>
+        <Link
+          to="/arena"
+          className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+        >
+          Arena
+        </Link>
+        <Link
+          to="/ranking"
+          className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+        >
+          Ranking
+        </Link>
+        <Link
+          to="/edition"
+          className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+        >
+          Edycja
+        </Link>
+        <Link
+          to="/arena"
+          className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+        >
+          Arena
+        </Link>
+        {userLoggedIn ? (
+          <Link
+            to="/"
+            className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+            onClick={() => {
+              setUserLoggedIn(null);
+              localStorage.removeItem('loggedUser');
+              enqueueSnackbar('Wylogowano pomyślnie', { variant: 'info' });
+            }}
+          >
+            Wyloguj
+          </Link>
+        ) : (
+          <>
+            <Link
+              to="/register"
+              className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+            >
+              Rejestracja
+            </Link>
+            <Link
+              to="/login"
+              className="text-white bg-pokeblue-500 hover:bg-pokeblue-700 transition-colors duration-300 px-4 py-2 rounded-sm"
+            >
+              Logowanie
+            </Link>
+          </>
+        )}
+
+        {/* PAMIĘTAJ o zablokowaniu przejścia pod adresy dla nizalogowanego użytkownika: Ulubione, Arena, Ranking, Edycja */}
+      </nav>
     </header>
   );
 };
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,10 @@ import MainLayout from '../components/layout/MainLayout';
 import Home from '../components/subpages/Home';
 import Register from '../components/subpages/Register';
 import Login from '../components/subpages/Login';
+import Arena from '../components/subpages/Arena';
+import Favourites from '../components/subpages/Favourites';
+import Ranking from '../components/subpages/Ranking';
+import Edition from '../components/subpages/Edition';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +19,10 @@ const router = createBrowserRouter([
       },
       { path: 'register', element: <Register /> },
       { path: 'login', element: <Login /> },
-      // tutaj dodać kolejne trasy: arena, favourites, edition itd.
+      { path: 'arena', element: <Arena /> },
+      { path: 'favourites', element: <Favourites /> },
+      { path: 'ranking', element: <Ranking /> },
+      { path: 'edition', element: <Edition /> },
     ],
   },
 ]);
